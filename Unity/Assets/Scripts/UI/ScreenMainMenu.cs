3a7bb5a88c62d686f171dff5810b0fbb8d6dff06
David Rubin <dudrubin@gmail.com>
2017-05-16 12:54:33 +0300

    Added back input logic

--- Unity/Assets/Scripts/UI/ScreenMainMenu.cs
+++ Unity/Assets/Scripts/UI/ScreenMainMenu.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScreenMainMenu : ScreenBase {
+    Button buttonSave;
+    InputField inputName;
 
 	void Awake () {
+        buttonSave = transform.Find("UserPanel/ButtonSave").GetComponent<Button>();
+        inputName = transform.Find("UserPanel/InputName").GetComponent<InputField>();
         transform.Find("ButtonLobby").GetComponent<Button>().onClick.AddListener(LobbyClicked);
         transform.Find("ButtonSettings").GetComponent<Button>().onClick.AddListener(SettingsClicked);
+        inputName.text = MainUser.Instance.Name;
+        buttonSave.onClick.AddListener(OnSave);
 	}
 
 	public void LobbyClicked() {
@@ -15,6 +21,10 @@ public class ScreenMainMenu : ScreenBase {
 
 	public void SettingsClicked() {
         MenusScene.GoTo(EScreenType.Settings);
+	}
+
+	public void OnSave() {
+        MainUser.Instance.Name = inputName.text;
 	}
 	
 }
