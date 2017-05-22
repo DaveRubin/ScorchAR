0b7f2b7b2bf562d1fecefd9e89eccdc84b2aef2c
David Rubin <dudrubin@gmail.com>
2017-05-22 23:04:16 +0300

    1.Moved camera to capture both tanks 2.Grouped Tanks\terrain to root object

--- Unity/Assets/Scripts/MainGame.cs
+++ Unity/Assets/Scripts/MainGame.cs
@@ -10,7 +10,7 @@ using Utils;
 
 public class MainGame : MonoBehaviour {
 
-    private const bool OFFLINE_MODE = false;
+    private const bool OFFLINE_MODE = true;
 
     private TankControl MyTank {
         get {
@@ -22,9 +22,13 @@ public class MainGame : MonoBehaviour {
     public CameraGUI Gui;
     public static ScorchEngine.Game GameCore;
     private int PlayerIndex;
+    private Transform rootTransform;
 
     void Awake() {
         PrefabManager.Init();
+        rootTransform = new GameObject().transform;
+        rootTransform.gameObject.name = "Root";
+        rootTransform.localPosition = Vector3.zero;
 
         if (OFFLINE_MODE) {
             MainUser.Instance.Name = "Test";
@@ -84,8 +88,10 @@ public class MainGame : MonoBehaviour {
 
         foreach (Player player in players) {
             TankControl tankGO = PrefabManager.InstantiatePrefab("Tank").GetComponent<TankControl>();
-            tankGO.transform.position = Vector3Extension.FromCoordinate(player.ControlledTank.Position);
+            tankGO.transform.SetParent(rootTransform);
+            tankGO.transform.localPosition = Vector3Extension.FromCoordinate(player.ControlledTank.Position);
             tankGO.SetPlayer(player);
+
             tanks.Add(tankGO);
         }
         Debug.Log(tanks);
@@ -99,9 +105,11 @@ public class MainGame : MonoBehaviour {
     /// TODO - should be replaced with Amitai's terrain object which will derive its topology from the game object
     /// </summary>
     public void CreateMockTerrain() {
+
         for (int x = 0; x < GameCore.Terrain.SizeX; x++) {
             for (int z = 0; z < GameCore.Terrain.SizeZ; z++) {
                 GameObject go = PrefabManager.InstantiatePrefab("Cube");
+                go.transform.SetParent(rootTransform);
                 go.transform.localPosition = new Vector3(x, 0, z);
             }
         }
