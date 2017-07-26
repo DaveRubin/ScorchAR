9c6a55bc0e56e64ba9064c1ee71b6ead3078815b
Amitai Handler <amitaihandler@gmail.com>
2017-07-26 21:34:05 +0300

    local commit

--- Unity/Assets/Scripts/Terrain/TerrainDeform.cs
+++ Unity/Assets/Scripts/Terrain/TerrainDeform.cs
@@ -8,17 +8,18 @@ public class TerrainDeform : MonoBehaviour {
 
     void OnGUI()
     {
-        if(GUI.Button(new Rect(30,30,200,30),"Deform Terrain"))
+        if(GUI.Button(new Rect(30,30,360,60),"Deform Terrain"))
         {
             //get the terrain heightmap width adn height
             int xSize = TerrainGameBoard.terrainData.heightmapWidth;
             int ySize = TerrainGameBoard.terrainData.heightmapHeight;
 
             //Generate a random crater when hitting the button
-            int radius = Random.Range(3,30);
-            int depth   = Random.Range(3,10);
-            int xExplosionPos = Random.Range(30, xSize - 30);
-            int yExplosionPos = Random.Range(30, ySize - 30);
+            int radius = Random.Range(1,3);
+            int depth   = Random.Range(0,4);
+            int xExplosionPos = Random.Range(3, xSize - 3);
+            int yExplosionPos = Random.Range(3, ySize - 3);
+            int varyingDepth = 0;
 
 
 
@@ -29,41 +30,25 @@ public class TerrainDeform : MonoBehaviour {
             //Manipulate the heights - deform the terrain
             for(int y = yExplosionPos - radius; y < yExplosionPos + radius; y++)
             {
+                varyingDepth = 0;
                 for(int x = xExplosionPos - radius; x < xExplosionPos + radius; x++)
                 {
-                    Debug.Log("current height at [" + x + "," + y + "] is: " + heights[x,y]);
-                    //heights[x,y] = heights[x,y] - (float)depth/ySize;
-                    heights[x,y] = 0;  
+                    //Debug.Log("current height at [" + x + "," + y + "] is: " + heights[x,y]);
+                    heights[x,y] = heights[x,y] - ((float)depth + varyingDepth)/ySize;
+                    if (x < xExplosionPos)
+                        varyingDepth++;
+                    else if (x > xExplosionPos)
+                        varyingDepth--;
+                    //heights[x,y] = 0;
                     if (heights[x,y] < 0)
                         heights[x,y] = 0;
-                    Debug.Log("after decrease height at [" + x + "," + y + "] is: " + heights[x,y]);
+                    //Debug.Log("after decrease height at [" + x + "," + y + "] is: " + heights[x,y]);
                 }
             }
-
-            /*
-            Debug.Log("current height at [" + xExplosionPos + "," + yExplosionPos + "] is: " + heights[xExplosionPos,yExplosionPos]);
-            Debug.Log("decreasing " + (float)depth/ySize + " from " + heights[xExplosionPos,yExplosionPos]);
-            heights[xExplosionPos,yExplosionPos] = heights[xExplosionPos,yExplosionPos] -  (float)depth/ySize;
-            if (heights[xExplosionPos,yExplosionPos] < 0)
-                heights[xExplosionPos,yExplosionPos] = 0;
-            Debug.Log("after decrease height at [" + xExplosionPos + "," + yExplosionPos + "] is: " + heights[xExplosionPos,yExplosionPos]);
-
-            heights[xExplosionPos - 1, yExplosionPos -1] = 0;
-            heights[xExplosionPos - 1, yExplosionPos] = 0;
-            heights[xExplosionPos - 1, yExplosionPos +1] = 0;
-            heights[xExplosionPos , yExplosionPos -1] = 0;
-            heights[xExplosionPos , yExplosionPos ] = 0;
-            heights[xExplosionPos , yExplosionPos +1] = 0;
-            heights[xExplosionPos + 1, yExplosionPos -1] = 0;
-            heights[xExplosionPos + 1, yExplosionPos ] = 0;
-            heights[xExplosionPos + 1, yExplosionPos +1] = 0;
-            */
-
 
             //SetHeights to perform change
             Debug.Log("changing height at " + xExplosionPos + " , " + yExplosionPos);
             TerrainGameBoard.terrainData.SetHeights(0,0,heights);
         }
     }
-
 }
