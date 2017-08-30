3f1e347c584abffc88eb72599d44fe237e17f112
Amitai Handler <amitaihandler@gmail.com>
2017-08-31 01:20:17 +0300

    adding music files etc

---
+++ Unity/Assets/Scripts/MusicScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour {
+
+    public AudioClip BGMusic;
+    public AudioSource AudioSource;
+
+
+	// Use this for initialization
+	void Start () {
+		AudioSource.clip = BGMusic;
+        AudioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
