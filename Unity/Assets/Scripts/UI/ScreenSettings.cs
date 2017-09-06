638ee25b9a8ff0b5f6beea7ea3d2ffffba353c4b
David Rubin <dudrubin@gmail.com>
2017-09-06 08:40:33 +0300

    Adjusted volume size and player prefs initial value

--- Unity/Assets/Scripts/UI/ScreenSettings.cs
+++ Unity/Assets/Scripts/UI/ScreenSettings.cs
@@ -4,19 +4,24 @@ using UnityEngine.UI;
 
 namespace UI {
     public class ScreenSettings : ScreenBase {
+        private const string VOLUME_KEY = "Volume";
+
         void Awake() {
             base.Awake();
-            Toggle toggle = transform.Find("Toggle").GetComponent<Toggle>();
+            //Toggle toggle = transform.Find("Toggle").GetComponent<Toggle>();
             Slider slider = transform.Find("Slider").GetComponent<Slider>();
             transform.Find("ButtonBack").GetComponent<Button>().onClick.AddListener(GoBack);
             int savedMuteVal = PlayerPrefs.GetInt("Mute");
             bool muteValBool = Convert.ToBoolean(savedMuteVal);
             AudioListener.pause = muteValBool;
-            float volume = PlayerPrefs.GetFloat("Volume");
+            if (!PlayerPrefs.HasKey(VOLUME_KEY)) {
+                PlayerPrefs.SetFloat(VOLUME_KEY,0.2f);
+            }
+            float volume = PlayerPrefs.GetFloat(VOLUME_KEY);
             slider.value = volume;
             AudioListener.volume = slider.value;
-            toggle.isOn = !muteValBool;
-            toggle.onValueChanged.AddListener(OnValueChange);
+            //toggle.isOn = !muteValBool;
+            //toggle.onValueChanged.AddListener(OnValueChange);
             slider.onValueChanged.AddListener(OnVolumeChange);
         }
 
